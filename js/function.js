0c2b2d0b55c71eae581036b6edb0dae0a6566a76
abdulahaddf <abdulahad.df1@gmail.com>
2023-02-18 14:13:43 +0600

    final

--- js/function.js
+++ js/function.js
@@ -4,64 +4,66 @@ document.getElementById('blogBtn').addEventListener('click', function(){
     window.location.href = './blog.html';  
 });
 
-
-
-
+// for Triangle
 document.getElementById('triBtn').addEventListener('click', function(){
-    serial ++;
    let area = areaType1('triBase', 'triHeight')
-   displayData("Triangle", area);
-  
+   if (area !== undefined) {
+    serial ++;}
+   displayData("Triangle", area); 
 });
+// for rectangle
 document.getElementById('recBtn').addEventListener('click', function(){
-    serial ++;
     let area = areaType2('rec1', 'rec2')
+    if (area !== undefined) {
+        serial ++;}
     displayData("Rectangle", area);
-  
-
 });
+
+// for Parallelogram
 document.getElementById('paraBtn').addEventListener('click', function(){
-    serial ++;
     let area = areaType2('para1', 'para2')
+    if (area !== undefined) {
+        serial ++;}
     displayData("Parallelogram", area);
-  
-
 });
 
+// for Rhombus
 document.getElementById('romBtn').addEventListener('click', function(){
-    serial ++;
     let area = areaType1('romd1', 'romd2')
+    if (area !== undefined) {
+        serial ++;}
     displayData("Rhombus", area);
-  
-    
-
 });
+
+// for Pentagon
 document.getElementById('pentaBtn').addEventListener('click', function(){
-    serial ++;
-    let area = areaType1('penta1', 'penta2')
+    let area = areaType1('penta1', 'penta2');
+    if (area !== undefined) {
+        serial ++;}
     displayData("Pentagon", area);
-  
-
 });
 
+// for Ellipse
 document.getElementById('elliBtn').addEventListener('click', function(){
-    serial ++;
+    
     const pi = 3.14;
     const a = document.getElementById('elli1').value;
     const b = document.getElementById('elli2').value;
     const area = pi * parseFloat(a) * parseFloat(b);
+    if (area !== undefined) {
+        serial ++;}
     displayData("Ellipse", area);
 });
 
+// function for calculating the area of triangle, rhombus, & pentagon
 function areaType1(id1,id2) {
     const b = document.getElementById(id1).value;
     const h = document.getElementById(id2).value;
     
     if (b == "" || h == "" || b <= 0 || h <= 0) {
       alert("Please enter a positive number for both the base and height.");
-      return; // Return nothing if there is an error.
+      return;
     }
-  
     const area = 0.5 * parseFloat(b) * parseFloat(h);
     return area;
   }
@@ -90,7 +92,7 @@ function displayData(name, area) {
     tr.innerHTML = `
       <td>${serial}</td>
       <td>${name}</td>
-      <td>${area.toFixed(2)} cm²</td>
+      <td>${area.toFixed(2)} cm<sup>2</sup></td>
       <td><button class="btn w-32 bg-sky-600 text-xs">Convert to m<sup>2</sup></button>
       </td>
       
@@ -98,7 +100,7 @@ function displayData(name, area) {
     container.appendChild(tr);
 
 }
-
+// function of generating random color
 function getRandomColor() {
     let symbols, color;
     symbols = "0123456789ABCDEF";
